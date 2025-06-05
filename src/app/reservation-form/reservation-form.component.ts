1462910b65f9bb92d5cff71a6f2b0034774b7498
Mebo <samkharadzemerab@gmail.com>
2025-06-05 15:49:22 +0400

    add delete and update operations on reservations list

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
@@ -7,19 +10,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private ReservationService: ReservationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
+      id: Date.now().toString(),
       checkInDate: ['', Validators.required],
       checkOutDate: ['', Validators.required],
       guestEmail: ['', [Validators.required, Validators.email]],
       guestName: ['', Validators.required],
       roomNumber: ['', Validators.required],
     });
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      let reservation = this.ReservationService.getReservation(id);
+      if (reservation) {
+        this.reservationForm.patchValue(reservation);
+      }
+    }
   }
   onSubmit() {
     if (this.reservationForm.valid) {
-      console.log('valid');
+      let reservation: Reservation = this.reservationForm.value;
+      this.ReservationService.addReservation(reservation);
+      this.router.navigate(['/list']);
     }
   }
 }
